3980cf6fc532cc8a7bfb5e87380596efdb9cb7f8
Evelin Matos <evelinmatoscd24@gmail.com>
2025-11-01 02:30:43 -0300

    Refatorando a estrutura do projeto com Routers e Annotated

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,7 @@ from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import User, table_registry
 from fast_zero.security import get_password_hash
+from fast_zero.settings import Settings
 
 
 @pytest.fixture
@@ -80,7 +81,12 @@ def user(session):
 @pytest.fixture
 def token(client, user):
     response = client.post(
-        '/token',
+        '/auth/token',
         data={'username': user.email, 'password': user.clean_password},
     )
     return response.json()['access_token']
+
+
+@pytest.fixture
+def settings():
+    return Settings()
